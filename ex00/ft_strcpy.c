0a4ee62b575af09169c8ea6186a89596234f11cd
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-11 12:13:48 +0200

    ft_strcpy

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 11:39:50 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/11 12:12:40 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int		main(void)
+{
+	char str1[] = "luna";
+	char str2[] = "hellogoodbye";
+
+	ft_strcpy(str2, str1);
+	return (0);
+}
