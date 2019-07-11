b0592f9b27abc5a3bd60701b68a7dc96939f58ff
LisaJongmans <50566830+LisaJongmans@users.noreply.github.com>
2019-07-11 19:24:19 +0200

    Update ft_str_is_alpha.c

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -20,7 +20,7 @@ int		ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] > 65 && str[i] < 90) || (str[i] > 97 && str[i] < 122))
+		if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
 			i++;
 		else
 			return (0);
