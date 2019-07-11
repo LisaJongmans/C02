cceff35e093c6232cebf55d80ae286e0f41a9c6d
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-11 20:03:09 +0200

    ft_strupcase

---
+++ ex07/ft_strupcase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 19:16:58 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/11 19:19:07 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strupcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int		main(void)
+{
+	char str[] = "al10%^gzy$";
+	ft_strupcase(str);
+	return 0;
+}
